870a3448b3f669572018fe9ae35e2a08875287e7
Jefferson Araujo <jefferson.silvaaraujo@hotmail.com>
2015-04-22 16:08:57 -0300

    Adição de panel
    - Adicionado panel para separação de cadastro
    - Adicionado opção para cadastro de pessoa física ou juridica

---
+++ Content/js/JsPages/cadastro_cliente.js
@@ -0,0 +1,28 @@
+$(document).ready(function()
+{
+	
+	/*
+	* Ativa cadastro para pessoa juridica
+	*/
+	$("#rdnJuridica").click(function()
+		{
+
+			$("#pnlFisica").fadeOut('slow',function()
+				{
+					$("#pnlJuridica").fadeIn('slow');
+				});
+
+		});
+
+	/*
+	* Ativa cadastro para pessoa física
+	*/
+	$("#rdnFisica").click(function()
+		{
+			$("#pnlJuridica").fadeOut('slow',function()
+				{
+					$("#pnlFisica").fadeIn('slow');
+				});
+		});
+
+});
