7b78ccf1ae0bae86b6b23e17e212f1a77f832f64
Jefferson Araujo <jefferson.silvaaraujo@hotmail.com>
2015-04-28 19:55:11 -0300

    Masterpage
    - Criado master page para chamar telas de cadastro
    - Ajustes em layout index

---
+++ Content/js/JsPages/master.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	var content = $('#content');
+	//pre carregando o gif
+	loading = new Image(); loading.src = '../Content/img/loading.gif';
+		
+	$('#menu div a').on('click', function( e ){
+		e.preventDefault();
+		content.html( '<div class="col-sm-5"></div><img class="loading" src="../Content/img/loading.gif" />' );
+ 
+		var href = $( this ).attr('href');
+		$.ajax({
+			url: href,
+			success: function( response ){
+				//forçando o parser
+				var data = $( response ).find('#content').html();
+ 
+				//apenas atrasando a troca, para mostrarmos o loading
+				 window.setTimeout( function(){
+				 	content.fadeOut('slow', function(){
+					 	content.html( data ).fadeIn();
+					 });
+					}, 500 );
+				}
+			});
+ 
+		});
+});
